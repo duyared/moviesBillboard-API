b31932c47ce78fc5412834468adf15c46a855643
yared <yaredte45@gmail.com>
2023-09-22 12:09:00 +0300

    add routes for user register and login,use express.json

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
+require('express-async-errors');
+
 const  express = require('express')
 const connectDB = require('./database/connect')
 const app = express()
+const authRouter = require('./routes/auth')
+
+app.use(express.json());
+
+
+//routes
+app.use('/api/v1/auth',authRouter)
 
 
 const port = process.env.PORT || 5000
