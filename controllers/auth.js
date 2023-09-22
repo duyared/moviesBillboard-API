775324db17722a972185b4a9c524cd5b4048140d
yared <yaredte45@gmail.com>
2023-09-22 12:07:24 +0300

    modified jwt secret name

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require("http-status-codes")
 const User = require("../models/User")
 
 const register = async (req,res) =>{
-    const user = await User.create(...req.body)
+    const user = await User.create({...req.body})
     const token = user.createJWT()
     res.status(StatusCodes.ACCEPTED).json({user:{name:user.name},token})
 }
